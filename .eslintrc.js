94543a1955d8ef971756ba5ea91de9fb063bb8a6
ot07 <ot07neoki@gmail.com>
2022-04-21 16:35:50 +0900

    🔧 テンプレートを更新

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
     node: true,
+    jest: true,
   },
   extends: [
     "eslint:recommended",
@@ -10,6 +11,9 @@ module.exports = {
     "plugin:react-hooks/recommended",
     "plugin:jsx-a11y/recommended",
     "plugin:import/recommended",
+    "plugin:jest/recommended",
+    "plugin:jest-dom/recommended",
+    "plugin:testing-library/react",
     "prettier",
   ],
   parserOptions: {
@@ -22,6 +26,17 @@ module.exports = {
     "no-var": "error",
     "import/no-unresolved": [2, { ignore: ["^src"] }],
     "no-unused-vars": "off",
+    "import/order": [
+      "warn",
+      {
+        "newlines-between": "always",
+        pathGroupsExcludedImportTypes: ["builtin"],
+        alphabetize: { order: "asc", caseInsensitive: true },
+        pathGroups: [
+          { pattern: "src/**", group: "internal", position: "before" },
+        ],
+      },
+    ],
     "unused-imports/no-unused-imports": "error",
     "unused-imports/no-unused-vars": [
       "warn",
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      roboto: ["Roboto", "sans-serif"],
+    },
+  },
+  plugins: [],
+  darkMode: "class",
+};
